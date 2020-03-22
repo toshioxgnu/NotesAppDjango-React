0177ae1070718dcdb356631584a84d5e1e3cba25
Jose Gonzalez <jgonzalezr5@outlook.es>
2020-03-22 11:44:53 -0300

    Agregando React para Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import {Button} from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          TEst
+          <Button color="danger">Hello i'm a button </Button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
